ded71e38c835f297963f97c9b503fb24497c22f3
Yousef Omar <yousefomar724@gmail.com>
2022-07-07 19:44:22 +0200

    replace getServerSide with getStaticSite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { Profile, FavIcon, Content } from '../components'
 import TopbarWithNoSSR from '../components/topbarWithNoSSR'
@@ -9,7 +9,7 @@ import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 import { motion } from 'framer-motion'
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const { posts, projects } = (await getHomePosts()) || []
   return {
     props: {
@@ -18,6 +18,7 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
       ...(await serverSideTranslations(locale!, ['home', 'common'])),
       locale,
     },
+    revalidate: 1,
   }
 }
 
